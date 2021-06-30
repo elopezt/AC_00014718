63e35e25022313e75d2135e06e640879e085824f
elopezt <00014718@uca.edu.sv>
2021-06-29 22:34:10 -0600

    Laboratorio 6

---
+++ Laboratorio 6/labo6.asm
@@ -0,0 +1,53 @@
+org 100h
+    section .text
+    mov DX,ingreso
+    call ingresando
+    mov BP,input
+
+    call cadena
+    call verificando
+    call ingresando
+
+    int     20h
+    section .data
+    clave DB "00014718"
+    
+    ingreso DB "Contraseña para ingresar: ", "$"
+    ingresoDenegado DB "Incorrecto", "$"
+    ingresoEfectivo DB "Bienvenido", "$"
+    input times 5 DB " "
+
+    awanta:
+        mov AH,01h
+        int 21h
+        ret
+    cadena:
+        XOR SI,SI   
+    while:
+        call awanta
+        cmp  AL,0x0D
+        je   exit
+        mov [BP+SI],AL
+        inc SI
+        jmp while
+    exit:
+        mov byte [BP+SI], "$"
+        ret
+    ingresando:
+        mov AH,09h
+        int 21h
+        ret
+    verificando:
+        mov AL,[clave]
+        mov AH,[input]
+        cmp AL,AH
+        je  bienvenido
+        jne incorrecto
+        ret
+    incorrecto:
+        mov     DX,ingresoDenegado
+        ret
+    bienvenido:
+        mov     DX,ingresoEfectivo
+        ret
+    
